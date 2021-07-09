ae0f04c905b14c3e265f94084791b65ba423c8af
Max Kalashnikov <maksimkalashnikov9129@gmail.com>
2021-07-09 09:38:00 +0300

    CHANGED | Added several menu areas for theme

--- lib/menu-areas.php
+++ lib/menu-areas.php
@@ -16,8 +16,11 @@ add_action( 'after_setup_theme', 'register_theme_menus' );
 function register_theme_menus() {
 	register_nav_menus(
 		array(
-			'primary'     => __( 'Primary Menu', 'wp_dev' ),
-			'footer_menu' => __( 'Footer Menu', 'wp_dev' ),
+			'primary'               => __( 'Primary Menu', 'casadeapostas' ),
+			'atulidade_footer_menu' => __( 'Atulidade Footer Menu', 'casadeapostas' ),
+			'apostas_footer_menu'   => __( 'Apostas Footer Menu', 'casadeapostas' ),
+			'contato_footer_menu'   => __( 'Contato Footer Menu', 'casadeapostas' ),
+			'documents_footer_menu'   => __( 'Documents Footer Menu', 'casadeapostas' ),
 		)
 	);
 }
