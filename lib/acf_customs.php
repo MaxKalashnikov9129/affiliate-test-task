c3032346a9e5f5a8da73063ca84d630b93c7f04e
Max Kalashnikov <maksimkalashnikov9129@gmail.com>
2021-07-09 09:36:12 +0300

    CHANGED | Added action to provide default setting for image field of ACF

--- lib/acf_customs.php
+++ lib/acf_customs.php
@@ -14,3 +14,13 @@ $acf_options_page_settings = array(
 	'capability' => 'edit_posts'
 );
 acf_add_options_page( $acf_options_page_settings );
+
+add_action('acf/render_field_settings/type=image', 'add_default_value_to_image_field');
+function add_default_value_to_image_field($field) {
+	acf_render_field_setting( $field, array(
+		'label'			=> 'Default Image',
+		'instructions'		=> 'Appears when creating a new post',
+		'type'			=> 'image',
+		'name'			=> 'default_value',
+	));
+}
